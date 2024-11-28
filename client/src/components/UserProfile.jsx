c581e962bd19ef1b83c18b82fc2e841a553af19b
priya036 <priyadharshinis.21aid@kongu.edu>
2024-11-28 14:28:55 +0530

    Updated profile page

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
+const apiUrl = process.env.REACT_APP_API_ENDPOINT;
+
 
 function UserProfile() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   useEffect(() => {
     if (isAuthenticated && user) {
-      axios.post('http://localhost:5000/saveUser', {
+      axios.post(`${apiUrl}/saveUser`, {
         name: user.name,
         email: user.email,
         picture: user.picture,
