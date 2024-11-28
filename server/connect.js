e9c18fdf3f311214a46f8980c2d64012a5803a63
priya036 <priyadharshinis.21aid@kongu.edu>
2024-11-28 14:53:30 +0530

    Added environment configuration

--- server/connect.js
+++ server/connect.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/ai', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
